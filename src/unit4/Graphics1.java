54510513091a125e067aa08dd97334e0fb460b44
Wonger123 <andrewwong817@gmail.com>
2022-04-05 09:47:01 -0400

    LayoutGraphiics.java

--- src/unit4/Graphics1.java
+++ src/unit4/Graphics1.java
@@ -9,39 +9,43 @@ import javax.swing.JPanel;
 public class Graphics1 {
 
 	public static void main(String[] args) {
-		
 		new Graphics1();
 	}
-	
-	// constructor
+
+	//constructor
 	Graphics1() {
-		// set up the graphics
+		//set up the graphics
 		JFrame window = new JFrame();
-		window.setSize(800, 700);
-		window.setTitle("First Graphics Program");
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // close window ends the program
-		// window.setResizable(false); // normally
-		window.setLocationRelativeTo(null); // centers the window
-	
-		JPanel panel = new JPanel();
-		// panel.setBackground(Color.YELLOW);
+		window.setSize(800,700);
+		window.setTitle("First Graphics program");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//window.setResizable(false); // normally
+		window.setLocationRelativeTo(null); //centres the window
 		
-		Color c1 = new Color (100, 0, 170); // each # is 0-255
-		Color c2 = Color.decode("#00CC3F");
+		//setup panel
+		JPanel panel = new JPanel();
+		
+		
+		//					  R   G  B
+		Color c1 = new Color(100, 0, 170);  //each # is 0-255
+									//green + some blue
+		Color c2 = Color.decode("#00CC3F"); 
+		//panel.setBackground(Color.YELLOW);
 		panel.setBackground(c2);
-		panel.add(new JLabel("Hello"));
-		JLabel label1 = new JLabel("Goodbye");
+		
+		panel.add(new JLabel("hello"));
+		JLabel label1 = new JLabel("GoodBye");
 		label1.setBackground(Color.WHITE);
 		label1.setOpaque(true);
-		label1.setForeground(Color.RED);
+		label1.setForeground(Color.RED);		
 		panel.add(label1);
 		
 		for (int i = 1; i <= 10; i++) {
-			panel.add(new JButton("Btn" + i));
+			panel.add(new JButton("Btn"+ i));
 		}
 		
-		window.add(panel);
+		window.add(panel);		
 		
-		window.setVisible(true); // this is the last thing to do
+		window.setVisible(true);  //this is the last thing you do (normally)
 	}
 }
---
+++ src/unit4/LayoutGraphics.java
@@ -0,0 +1,60 @@
+package unit4;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/*  --- Layout Information ---
+ * JFrame defaults to BorderLayout
+ *    BorderLayout.CENTER is the default location when you add things
+ *    BorderLayout.CENTER will grow to fill all available space
+ * JPanel defaults to FlowLayout
+ * 	  FlowLayout wraps depending on the width of the window.
+ *    It defaults to being centred in the window.
+ * 
+ * All layouts can be created with horizontal and vertical gaps.
+ */
+public class LayoutGraphics extends JFrame {
+
+	public static void main(String[] args) {
+		new LayoutGraphics();
+	}
+
+	/* Global variables */
+	
+	//You can create the object here or in your setup part of the program.
+	JPanel panelA;
+	JPanel panelB = new JPanel();
+	
+	LayoutGraphics(){
+		this.setSize(800,700);
+		this.setTitle("Layouts");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null); //centres the window
+		
+		/** set up panel A **/
+		panelA = new JPanel();
+		panelA.setBackground(Color.PINK);
+		//panelA.setSize(400, 700); //????
+		panelA.setPreferredSize(new Dimension(400, 500));
+		this.add(panelA, BorderLayout.WEST);
+		
+		/** set up panel B **/
+		panelB.setBackground(Color.GREEN);
+		
+		//4 rows, 2 cols, 10 pixel gaps between buttons
+		panelB.setLayout(new GridLayout( 4,2, 10,10));
+		for (int i=0; i < 8; i++) {
+			panelB.add(new JButton("A button"));
+		}
+		
+		this.add(panelB, BorderLayout.EAST);
+		
+		this.pack(); //optional. Make the window as small as possible.
+		this.setVisible(true);
+	}
+}
