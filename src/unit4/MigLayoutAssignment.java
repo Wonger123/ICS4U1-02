9f5a5dd780dc78dde382ab52483918f4e38efc69
Wonger123 <andrewwong817@gmail.com>
2022-04-09 17:39:19 -0400

    Mig Stuff.

--- src/unit4/MigLayoutAssignment.java
+++ src/unit4/MigLayoutAssignment.java
@@ -2,15 +2,15 @@ package unit4;
 
 import java.awt.Color;
 import java.awt.GridLayout;
-
+import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
-import javax.swing.border.Border;
-
+import javax.swing.border.TitledBorder;
 import net.miginfocom.swing.MigLayout;
 
 public class MigLayoutAssignment extends JFrame {
@@ -43,10 +43,14 @@ public class MigLayoutAssignment extends JFrame {
 		p.add(new JLabel("Phone:"), "align right");
 		p.add(new JTextField(15), "wrap");
 
-		p.add(new JLabel("Address:"), "align right");
+		p.add(new JLabel("Address:"), "align right, gapbottom 20");
 		p.add(new JTextField(30), "span 2, wrap");
 
+		// size radio buttons
 		JPanel radioSize = new JPanel(new GridLayout(3, 1));
+		radioSize.setBackground(Color.LIGHT_GRAY);
+		radioSize.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Size",
+				TitledBorder.LEFT, TitledBorder.TOP));
 		JRadioButton small = new JRadioButton("Small");
 		JRadioButton medium = new JRadioButton("Medium");
 		JRadioButton large = new JRadioButton("Large");
@@ -65,7 +69,10 @@ public class MigLayoutAssignment extends JFrame {
 
 		p.add(radioSize, "align center, top");
 
+		// style radio buttons
 		JPanel radioStyle = new JPanel(new GridLayout(2, 1));
+		radioStyle.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Style",
+				TitledBorder.LEFT, TitledBorder.TOP));
 		JRadioButton thin = new JRadioButton("Thin");
 		JRadioButton thick = new JRadioButton("Thick");
 
@@ -81,7 +88,10 @@ public class MigLayoutAssignment extends JFrame {
 
 		p.add(radioStyle, "align center, top");
 
+		// toppings radio buttons
 		JPanel radioToppings = new JPanel(new GridLayout(5, 1));
+		radioToppings.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Toppings",
+				TitledBorder.LEFT, TitledBorder.TOP));
 		JRadioButton pepperoni = new JRadioButton("Pepperoni");
 		JRadioButton mushrooms = new JRadioButton("Mushrooms");
 		JRadioButton olives = new JRadioButton("Olives");
@@ -105,5 +115,11 @@ public class MigLayoutAssignment extends JFrame {
 		radioToppings.add(peppers);
 
 		p.add(radioToppings, "align center, top");
+
+		JButton btn1 = new JButton("Ok");
+		JButton btn2 = new JButton("Close");
+
+		p.add(btn1, "skip 2, align right, split 2");
+		p.add(btn2, "align right");
 	}
 }
